0e17581a2339c01c8e4b58b050a4453e82e27caa
Пользователь <kuleshov000@gmail.com>
2023-04-09 21:59:18 +0300

    add modal

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,49 @@
 'use strict'
 
+const body = document.querySelector('body');
 const menu = document.querySelector('.menu');
 const burger = document.querySelector('.header__burger');
-const body = document.querySelector('body');
+const loginBtn = document.querySelector('.menu__btn');
+const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal__overlay');
+const close = document.querySelector('.modal__close');
+const loginInput = document.querySelector('#login');
+
+const overflowBody = (element) => {
+    if (element.classList.contains('active')) {
+        body.style.overflow = 'hidden';
+    } else {
+        body.style.overflow = 'auto';
+    }
+}
+
+const removeActive = (element) => {
+    if (element.classList.contains('active')) {
+        element.classList.remove('active');
+    }
+}
+
+const closeModal = () => {
+    modal.classList.remove('active');
+    overflowBody(modal);
+}
 
 burger.addEventListener('click', () => {
     menu.classList.toggle('active');
     burger.classList.toggle('active');
 
-    if (menu.classList.contains('active')) {
-        body.style.overflow = 'hidden';
-    } else {
-        body.style.overflow = 'auto';
-    }
+    overflowBody(menu);
 });
+
+loginBtn.addEventListener('click', () => {
+    modal.classList.add('active');
+    loginInput.focus();
+
+    removeActive(menu);
+    removeActive(burger);
+    overflowBody(modal);
+});
+
+close.addEventListener('click', closeModal);
+
+modalOverlay.addEventListener('click', closeModal);
